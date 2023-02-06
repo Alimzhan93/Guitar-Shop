de7136d6f94980e50f9c40b0832c060eb73a4a84
77001 <alimzhanzhakashev@gmail.com>
2023-02-06 19:25:31 +0600

    init commit

--- utils/localStorageUtil.js
+++ utils/localStorageUtil.js
@@ -1,28 +1,32 @@
-class localStorageUtil{
-    constructor(){
+class LocalStorageUtil {
+    constructor() {
         this.keyName = 'products';
     }
 
     getProducts() {
-        const productsLocatStorage = localStorage.getItem(this.keyName);
-        if(productsLocatStorage !== null){
-            return JSON.parse(productsLocatStorage);
+        const productsLocalStorage = localStorage.getItem(this.keyName);
+        if (productsLocalStorage !== null) {
+            return JSON.parse(productsLocalStorage);
         }
         return [];
     }
-    
-    putProducts(id){
-    
+
+    putProducts(id) {
+        let products = this.getProducts();
+        let pushProduct = false;
+        const index = products.indexOf(id);
+
+        if (index === -1) {
+            products.push(id);
+            pushProduct = true;
+        } else {
+            products.splice(index, 1);
+        }
+
+        localStorage.setItem(this.keyName, JSON.stringify(products));
+
+        return { pushProduct, products }
     }
-
-
 }
 
-
-
-
-const localStorageUtil = new localStorageUtil();
-
-const a = localStorageUtil.getProducts();
-
-console.log(a);
+const localStorageUtil = new LocalStorageUtil();
